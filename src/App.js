ab3dce6e65a44edc35382b4592328fed614182aa
MaksimBoyko05 <vladosmakasin1@gmail.com>
2024-12-20 11:14:17 +0200

    completed graphQL app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import { ApolloClient, InMemoryCache, ApolloProvider, useQuery, gql } from '@apollo/client';
 
-function App() {
+const client = new ApolloClient({
+  uri: 'https://beta.pokeapi.co/graphql/v1beta',
+  cache: new InMemoryCache(),
+});
+
+const GET_POKEMONS = gql`
+  query GetPokemons($limit: Int!) {
+    pokemon_v2_pokemon(limit: $limit) {
+      id
+      name
+      height
+      weight
+    }
+  }
+`;
+
+const PokemonList = () => {
+  const [limit, setLimit] = useState(10);
+
+  const { loading, error, data } = useQuery(GET_POKEMONS, {
+    variables: { limit },
+  });
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error: {error.message}</p>;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Pokemon List</h1>
+      <label>
+        Number of Pokemons to Display:
+        <input
+          type="number"
+          value={limit}
+          onChange={(e) => setLimit(parseInt(e.target.value, 10))}
+        />
+      </label>
+      <ul>
+        {data.pokemon_v2_pokemon.map((pokemon) => (
+          <li key={pokemon.id}>
+            <strong>{pokemon.name}</strong> (Height: {pokemon.height}, Weight: {pokemon.weight})
+          </li>
+        ))}
+      </ul>
     </div>
   );
-}
+};
+
+const App = () => (
+  <ApolloProvider client={client}>
+    <PokemonList />
+  </ApolloProvider>
+);
 
 export default App;
